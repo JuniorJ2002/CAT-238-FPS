972e3e58400b484e7c4a8a6548f76b96eeafc5b5
JuniorJ2002 <junior_julca@bloomfield.edu>
2022-04-11 18:35:11 -0400

    4/11/22

--- FPS/Assets/Project/Scripts/Game/GameManager.cs
+++ FPS/Assets/Project/Scripts/Game/GameManager.cs
@@ -7,14 +7,20 @@ public class GameManager : MonoBehaviour
 {
     [Header("Game")]
     public Player player;
+    public GameObject enemySpawner;
+
     [Header("UI")]
     public Text ammoText;
     public Text healthText;
+    public Text enemyText;
+    public Text infoText;
+
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        infoText.gameObject.SetActive(false); // Disabling info text
     }
 
     // Update is called once per frame
@@ -22,5 +28,30 @@ public class GameManager : MonoBehaviour
     {
         ammoText.text = "Ammo: " + player.Ammo;
         healthText.text = "Health: " + player.Health;
+        
+
+        int KilledEnemies = 0;
+        foreach(Enemy enemy in enemySpawner.GetComponentsInChildren<Enemy>())
+        {
+            //We are going to iterate over every enemy
+            if(enemy.Killed == false)
+            {
+                KilledEnemies++;
+            }
+        }
+        enemyText.text = "Enemies: " + KilledEnemies;
+
+        //Win Condition
+        if(KilledEnemies == 0)
+        {
+            infoText.gameObject.SetActive(true);
+            //infoText.text = "You Win!\n Good Job!";
+        }
+        //Lose Condition
+        if (player.Killed == true)
+        {
+            infoText.gameObject.SetActive(true);
+            infoText.text = "You Lose!";
+        }
     }
 }
